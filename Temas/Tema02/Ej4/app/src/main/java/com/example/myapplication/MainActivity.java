e5a73ab94e0ccd542ad6c1db21585685f49810f4
Xabier Gabiña <xabierland@gmail.com>
2025-01-23 17:06:10 +0100

    update

--- Temas/Tema02/Ej4/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Temas/Tema02/Ej4/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,18 +7,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button button = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear y aplicar la animación de rotación
+                RotateAnimation rotateAnimation = new RotateAnimation(
+                    0f, 45f,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF, 0.5f
+                );
+                rotateAnimation.setDuration(500);
+                rotateAnimation.setFillAfter(true);
+                textView.startAnimation(rotateAnimation);
+            }
         });
     }
 }
--- Temas/Tema02/Ej5/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Temas/Tema02/Ej5/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,18 +7,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Log.i("onCreate","Paso por onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("onStart","Paso por onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("onResume","Paso por onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("onPause","Paso por onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("onStop","Paso por onStop");
     }
 }
