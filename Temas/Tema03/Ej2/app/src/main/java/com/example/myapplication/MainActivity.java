ffcca71f619b5277a18eaa881c51f7ea62492c16
Xabier Gabiña <xabierland@gmail.com>
2025-02-04 16:12:00 +0100

    update

---
+++ Temas/Tema03/Ej2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        //Encontrar el objeto button y que lea el editText y abra la URL que esta escrita mediante un intent
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = findViewById(R.id.editText);
+                String url = editText.getText().toString().trim();
+                
+                if (!url.isEmpty()) {
+                    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                        url = "https://" + url;
+                    }
+                    try {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        startActivity(intent);
+                    } catch (Exception e) {
+                        // Manejar el error
+                    }
+                }
+            }
+        });
+    }
+}
